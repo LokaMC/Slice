63f80527a3b3b304e92dcff4a08c03ac66a82e9b
Cryptite <cryptite@gmail.com>
2026-01-22 07:58:27 -0600

    add EntityTryLungeEvent, and another patch while we wait for upstream PRs to ever be merged

---
+++ fork-api/src/main/java/io/papermc/paper/event/entity/EntityTryLungeEvent.java
@@ -0,0 +1,42 @@
+package io.papermc.paper.event.entity;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityEvent;
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * Event for when a LivingEntity attempts a Lunge "maybe". Cancelling this is possibly not guaranteed to work.
+ */
+@NullMarked
+public class EntityTryLungeEvent extends EntityEvent implements Cancellable {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private boolean cancelled;
+
+    @ApiStatus.Internal
+    public EntityTryLungeEvent(final LivingEntity entity) {
+        super(entity);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+}
